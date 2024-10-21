718a52c4c813ff4701342b58b87813ae584c1f11
ericksebastian2003 <151951589+ericksebastian2003@users.noreply.github.com>
2024-10-21 17:50:56 -0500

    Ionic

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {PhotoService} from '../services/photo.service';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
-
+  constructor(public photoService: PhotoService){}
+  async ngOnInit(){
+    await this.photoService.loadSaved();
+  }
+  addPhotoToGallery(){
+    this.photoService.addNewToGallery();
+}
 }
